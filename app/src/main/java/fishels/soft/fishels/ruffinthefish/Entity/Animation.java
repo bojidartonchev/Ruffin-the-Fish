2bc6e6d7afd9723f478f80943835bd60f7d515b3
Bozhidar Tonchev <bojidar.tonchev@gmail.com>
2016-01-12 12:57:42 +0200

    Copyright added.

--- app/src/main/java/fishels/soft/fishels/ruffinthefish/Entity/Animation.java
+++ app/src/main/java/fishels/soft/fishels/ruffinthefish/Entity/Animation.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright © 2015 Ruffin the Fish
+ *
+ * This file is part of "Ruffin the Fish".
+ *
+ * "Ruffin the Fish" is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * "Ruffin the Fish" is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with "Ruffin the Fish".  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package fishels.soft.fishels.ruffinthefish.Entity;
 
 import android.graphics.Bitmap;
